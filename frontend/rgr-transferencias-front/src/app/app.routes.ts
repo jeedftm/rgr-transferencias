5f70797ad440c939c2f0b5e9994a329d78060028
Jedson Batista <jedsonbatista@gmail.com>
2025-10-22 02:26:48 -0300

    Frontend Angular integrado com backend - listagem e agendamento de transferências rodandoo

--- frontend/rgr-transferencias-front/src/app/app.routes.ts
+++ frontend/rgr-transferencias-front/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TransferenciasComponent } from './components/transferencias/transferencias.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TransferenciasComponent }
+];
